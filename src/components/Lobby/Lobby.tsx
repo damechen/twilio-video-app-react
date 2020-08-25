7694e17b0bb709770123116db93e9f1b4b58493f
Damon Chen <damon@Damons-MacBook-Air.local>
2020-08-24 21:49:44 -0700

    lobby image

--- src/components/Lobby/Lobby.tsx
+++ src/components/Lobby/Lobby.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Lobby = () => {
   return (
-    <div className="min-h-screen bg-white flex">
+    <div className="h-screen bg-white flex">
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-40">
         <div className="mx-auto w-full max-w-sm lg:w-auto">
           <div>
